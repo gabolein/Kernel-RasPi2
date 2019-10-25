0a86075368962323a51d91e9c3aafc62a0e5fb35
Thomas <thomas.fock@campus.tu-berlin.de>
2019-10-25 17:40:50 +0200

    Debugging prints

--- Aufgabe1/vorgabe/system/start.c
+++ Aufgabe1/vorgabe/system/start.c
@@ -4,10 +4,34 @@
 
 void start_kernel(void)
 { 
-  kputChar('G');
-  kputChar('\n');
-  kprintf("Ich bin ein Brot\n");
-  kprintf("Ich bin ein Brot\n");
+  //kputChar('G');
+  //kputChar('\n');
+  //kprintf("123456789123456789123456789");
+  //kprintf("Ich bin ein Brot\n");
+
+  initUart();
+  
+  kputChar('a');
+  kputChar('b');
+  kputChar('c');
+  kputChar('d');
+  kputChar('e');
+  kputChar('f');
+  kputChar('g');
+  kputChar('h');
+  kputChar('i');
+  kputChar('j');
+  kputChar('k');
+  kputChar('l');
+  kputChar('m');
+  kputChar('n');
+  kputChar('o');
+  kputChar('p');
+  kputChar('q');
+  kputChar('r');
+  kputChar('s');
+  kputChar('t');
+
   
   while(1){
   }
