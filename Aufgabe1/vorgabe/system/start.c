ffe977618c5a295558c1f9f0062b0e45d50bf6b3
Thomas <thomas.fock@campus.tu-berlin.de>
2019-10-26 17:12:18 +0200

    Remove debug prints

--- Aufgabe1/vorgabe/system/start.c
+++ Aufgabe1/vorgabe/system/start.c
@@ -3,39 +3,11 @@
 #include "kio.h"
 
 void start_kernel(void)
-{ 
-  //kputChar('G');
-  //kputChar('\n');
-  //kprintf("123456789123456789123456789");
-  //kprintf("Ich bin ein Brot\n");
-
-  initUart();
-
-  //yellow_on();
-  //yellow_off();
-  
-  
-  kputChar('a');
-  kputChar('b');
-  kputChar('c');
-  kputChar('d');
-  kputChar('e');
-  kputChar('f');
-  kputChar('g');
-  kputChar('h');
-  kputChar('i');
-  kputChar('j');
-  kputChar('k');
-  kputChar('l');
-  kputChar('m');
-  kputChar('n');
-  kputChar('o');
-  kputChar('p');
-  kputChar('q');
-  kputChar('r');
-  kputChar('s');
-  kputChar('t');
-  
+{
+  char receivedChar;
   while(1){
+    kputChar('G');
+    kputChar('\n');
+      
   }
 }
