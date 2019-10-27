36298d62cafc52c0932b0b43df087f98cf9356ef
Thomas Fock <thomas.fock@posteo.de>
2019-10-27 19:00:56 +0100

    Add handlers file to compilation process

---
+++ Aufgabe1/vorgabe/system/handler.c
@@ -0,0 +1,23 @@
+#include "handler.h"
+
+void undefined_instruction(){
+  
+}
+void software_interrupt(){
+  
+}
+void prefetch_abort(){
+  
+}
+void data_abort(){
+  
+}
+void not_used(){
+  
+}
+void irq(){
+  
+}
+void fiq(){
+  
+}
