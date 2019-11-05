93bcb73580e460d74d0c57a31c8a0a13865dbb16
Thomas Fock <thomas.fock@posteo.de>
2019-11-05 01:01:46 +0100

    Add registerDump structs and formatting function

--- Aufgabe1/vorgabe/include/presentations.h
+++ Aufgabe1/vorgabe/include/presentations.h
@@ -1,11 +1,69 @@
-//
-// Created by gabolein on 30.10.19.
-//
+#include <stdint.h>
 
 #ifndef _PRESENTATIONS_H
 #define _PRESENTATIONS_H
 
+enum AccessStyle {
+        READ,
+        WRITE,
+        NONE
+};
+
+enum ExceptionType {
+        SOFTWARE_INTERRUPT,
+        DATA_ABORT,
+        UNDEFINED_INSTRUCTION,
+        PREFETCH_ABORT,
+        IRQ,
+        FIQ
+};
+
+struct regDump {
+        enum ExceptionType exType;
+        uint32_t insAddress;
+        enum AccessStyle accessStyle; 
+        uint32_t accessAddress;
+        char* faultName;
+        uint32_t r0;
+        uint32_t r1;
+        uint32_t r2;
+        uint32_t r3;
+        uint32_t r4;
+        uint32_t r5;
+        uint32_t r6;
+        uint32_t r7;
+        uint32_t r8;
+        uint32_t r9;
+        uint32_t r10;
+        uint32_t r11;
+        uint32_t r12;
+        uint32_t sp;
+        uint32_t lr;
+        uint32_t pc;
+        uint32_t cpsr;
+        uint32_t spsr;
+        uint32_t userLr;
+        uint32_t userSp;
+        uint32_t supervisorLr;
+        uint32_t supervisorSp;
+        uint32_t supervisorSpsr;
+        uint32_t abortLr;
+        uint32_t abortSp;
+        uint32_t abortSpsr;
+        uint32_t fiqLr;
+        uint32_t fiqSp;
+        uint32_t fiqSpsr;
+        uint32_t irqLr;
+        uint32_t irqSp;
+        uint32_t irqSpsr;
+        uint32_t undefinedLr;
+        uint32_t undefinedSp;
+        uint32_t undefinedSpsr;
+};
+
 void inputPresentation(char receivedChar);
 void specialMessage(const char*);
+
+void printRegisterDump(struct regDump*);
 
 #endif
--- Aufgabe1/vorgabe/lib/presentations.c
+++ Aufgabe1/vorgabe/lib/presentations.c
@@ -20,3 +20,40 @@ void specialMessage(const char* msg){
         kprintf("%s\n", msg);
         kprintf("---------------------------\n\n\n");    
 }
+
+void registerDump(struct regDump* regDump){
+        kprintf("#################################### \
+#######################################\n");
+
+        switch(regDump->exType) {
+        case SOFTWARE_INTERRUPT:    kprintf("Software Interrupt"); break;
+        case DATA_ABORT:            kprintf("Data Abort"); break;
+        case UNDEFINED_INSTRUCTION: kprintf("Undefined Instruction"); break;
+        case PREFETCH_ABORT:        kprintf("Prefetch Abort"); break;
+        case IRQ:                   kprintf("Interrupt"); break;
+        case FIQ:                   kprintf("Fast Interrupt"); break;
+        default:                    kprintf("Undefined Interrupt"); break; /*Error*/
+        }
+
+        kprintf(" an Addresse %x\n", regDump->insAddress);
+        
+        if(regDump->accessAddress != NONE){
+                if(regDump->accessStyle == READ){
+                        kprintf("Zugriff: lesend auf Addresse %x\n", regDump->accessAddress);
+                } else {
+                        kprintf("Zugriff: schreibend auf Addresse %x\n", regDump->accessAddress);
+                }
+        }
+        kprintf("Fehler: %s\n\n", regDump->faultName);
+        kprintf(">>> Registerschnappschuss (aktueller Modus) <<<\n");
+        kprintf("R0: %x  R8:  %x\n", regDump->r0, regDump->r8);
+        kprintf("R1: %x  R9:  %x\n", regDump->r1, regDump->r9);
+        kprintf("R2: %x  R10: %x\n", regDump->r2, regDump->r10);
+        kprintf("R3: %x  R11: %x\n", regDump->r3, regDump->r11);
+        kprintf("R4: %x  R12: %x\n", regDump->r4, regDump->r12);
+        kprintf("R5: %x  SP:  %x\n", regDump->r5, regDump->sp);
+        kprintf("R6: %x  LR:  %x\n", regDump->r6, regDump->lr);
+        kprintf("R7: %x  PC:  %x\n", regDump->r7, regDump->pc);
+
+        kprintf("\n\n>>> Aktuelle Statusregister (SPSR des aktuellen Modus) <<<\n");
+}
