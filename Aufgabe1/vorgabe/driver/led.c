38402e3c1be1ac559c741f1ea657270104d1713d
Gabo Behrendt <gabo.behrendt@gmail.com>
2019-11-05 18:22:58 +0100

    Implement usage of other leds

--- Aufgabe1/vorgabe/driver/led.c
+++ Aufgabe1/vorgabe/driver/led.c
@@ -1,5 +1,9 @@
 #define GPIO_BASE (0x7E200000 - 0x3F000000)
+#define GREEN0_LED 4
+#define YELLOW0_LED 5
+#define RED_LED 6
 #define YELLOW_LED 7
+#define GREEN_LED 8
 #define GPF_BITS 3
 
 enum gpio_func {
@@ -29,3 +33,56 @@ void yellow_on(void)
 void yellow_off(void) {
     gpio_port->clr[0] = 1 << YELLOW_LED;
 }
+
+void green_on(void)
+{
+        /* Initialisieren */
+        gpio_port->func[0] = gpio_output << (GREEN_LED * GPF_BITS);
+
+        /* Anschalten */
+        gpio_port->set[0] = 1 << GREEN_LED;
+}
+
+void green_off(void) {
+        gpio_port->clr[0] = 1 << GREEN_LED;
+}
+
+void red_on(void)
+{
+        /* Initialisieren */
+        gpio_port->func[0] = gpio_output << (RED_LED * GPF_BITS);
+
+        /* Anschalten */
+        gpio_port->set[0] = 1 << RED_LED;
+}
+
+void red_off(void) {
+        gpio_port->clr[0] = 1 << RED_LED;
+}
+
+void yellow0_on(void)
+{
+        /* Initialisieren */
+        gpio_port->func[0] = gpio_output << (YELLOW0_LED * GPF_BITS);
+
+        /* Anschalten */
+        gpio_port->set[0] = 1 << YELLOW0_LED;
+}
+
+void yellow0_off(void) {
+        gpio_port->clr[0] = 1 << YELLOW0_LED;
+}
+
+void green0_on(void)
+{
+        /* Initialisieren */
+        gpio_port->func[0] = gpio_output << (GREEN0_LED * GPF_BITS);
+
+        /* Anschalten */
+        gpio_port->set[0] = 1 << GREEN0_LED;
+}
+
+void green0_off(void) {
+        gpio_port->clr[0] = 1 << GREEN0_LED;
+}
+
--- Aufgabe1/vorgabe/lib/tests.c
+++ Aufgabe1/vorgabe/lib/tests.c
@@ -3,8 +3,14 @@
 //
 
 #include "tests.h"
+#include "kio.h"
 
 void causeDataAbort() {
 	int* a = (int*)0x1;
+        kprintf("%x\n", *a);
+}
+
+void causePrefetchDataAbort() {
+        int* a (int*)0xaaaaaaaaa;
         kprintf("%x\n", *a);
 }
