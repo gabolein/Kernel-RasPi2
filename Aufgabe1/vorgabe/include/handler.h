a8ee892045ec6682830a3a21a16b51fcc9cea38c
Thomas Fock <thomas.fock@posteo.de>
2019-11-06 19:25:57 +0100

    Add getDFSRReg

--- Aufgabe1/vorgabe/include/handler.h
+++ Aufgabe1/vorgabe/include/handler.h
@@ -1,5 +1,15 @@
+#include <stdint.h>
+
 #ifndef _BAR_H_
 #define _BAR_H_
+
+/*
+enum SFSRStatus {
+
+};
+*/
+
+uint32_t getDFSRReg();
 
 extern void undefined_instruction();
 extern void software_interrupt();
@@ -8,6 +18,10 @@ extern void data_abort();
 extern void not_used();
 extern void irq();
 extern void fiq();
+
+
+
+
 
 #endif
 
--- Aufgabe1/vorgabe/system/handler.c
+++ Aufgabe1/vorgabe/system/handler.c
@@ -3,12 +3,26 @@
 #include "hwDefines.h"
 #include "kio.h"
 #include "serial.h"
+#include <stdint.h>
 
 /* Register Defs */
 static volatile uint32_t* uart_icr  = UART_ICR;
 
 static volatile uint32_t* irq_pending_2 = IRQ_PENDING_2;
 /* Register Defs End */
+
+
+/* General Procedure:
+   Get Contents of DFSR in C variable using inline assembly.
+
+ */
+
+uint32_t getDFSRReg(){
+        uint32_t dfsr;
+        asm volatile("mcr p15, 0, %0, c5, c0, 0" : "=r" (dfsr));
+        return dfsr;
+}
+
 
 void undefined_instruction(){
         green_on();
@@ -21,21 +35,23 @@ void prefetch_abort(){
 }
 void data_abort(){
         yellow_on();
+
+        kprintf("%x\n", getDFSRReg());
 }
 void not_used(){
   
 }
 void irq(){ 
- 	//yellow_on();
+        //yellow_on();
         /* Check for pending UART Interrupt */
-  	if(*irq_pending_2 & (uint32_t)(1 << 25)){ 
+        if(*irq_pending_2 & (uint32_t)(1 << 25)){
                 char receivedChar;
                 uartReceiveChar(&receivedChar);
-		kprintf("This character caused an interrupt: %c\n\n\n", receivedChar);
-  	}
+                kprintf("This character caused an interrupt: %c\n\n\n", receivedChar);
+        }
         /* Clear all Interrupt state bits */
-  	*uart_icr = 0; 
-	kprintf("I am useless now :D. Plz end me\n");
+        *uart_icr = 0;
+        kprintf("I am useless now :D. Plz end me\n");
         /* Abschmieren */
         while(1);
 }
