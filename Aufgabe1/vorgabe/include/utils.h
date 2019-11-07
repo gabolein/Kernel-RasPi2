3f9c3ee7ddf337dae7cd12d242da6383fc68d06e
Gabo Behrendt <gabo.behrendt@gmail.com>
2019-11-07 17:20:47 +0100

    Change itoa to have buffer as argument

--- Aufgabe1/vorgabe/include/utils.h
+++ Aufgabe1/vorgabe/include/utils.h
@@ -1,5 +1,6 @@
 #ifndef _UTILS_H
 #define _UTILS_H
-char* itoa10(unsigned int val);
-char* itoa16(unsigned int val);
+char* itoa10(unsigned int val, char* c_buffer);
+char* itoa16(unsigned int val, char* c_buffer);
+void setEmptyString(char c_buffer[], int buf_len);
 #endif //_UTILS_H
--- Aufgabe1/vorgabe/lib/kio.c
+++ Aufgabe1/vorgabe/lib/kio.c
@@ -4,10 +4,13 @@
 
 #include "kio.h"
 #include "serial.h"
+#include "utils.h"
 #include <stdarg.h>
 #include <stdint.h>
 
 #define MIN_INT -2147483648
+#define LEN_DEC 10 // max chars needed to represent decimal
+#define LEN_HEX 8 // max chars needed to represent hex
 
 /*
  * Prints a string in the given format to screen
@@ -15,96 +18,72 @@
 __attribute__((format(printf, 1, 2)))
 void kprintf(const char* format, ...) {
 
-    va_list arg;
-    va_start(arg, format);
+        va_list arg;
+        va_start(arg, format);
 
-    unsigned char char_rep;
-    const char* str;
-    unsigned int u_int_num;
-    int int_num;
-    unsigned int address;
+        unsigned char char_rep;
+        const char* str;
+        unsigned int u_int_num;
+        int int_num;
+        unsigned int address;
+        char c_buffer10[LEN_DEC];
+        char c_buffer16[LEN_HEX];
 
 
-    for (const char* traverse = format; *traverse != '\0'; traverse++) {
-        if (*traverse == '%') {
-            traverse++; // move to next character, we dont wanna print the '%'
+        for (const char* traverse = format; *traverse != '\0'; traverse++) {
+                if (*traverse == '%') {
+                        traverse++; // move to next character, we dont wanna print the '%'
+                        switch(*traverse) {
+                                case '%' :
+                                        kputChar('%');
+                                        break;
+                                case 'c' :
+                                        char_rep = va_arg(arg, int);
+                                        kputChar(char_rep);
+                                        break;
+                                case 's' :
+                                        str = va_arg(arg, const char*);
+                                        kprintf(str);
+                                        break;
+                                case 'x' :
+                                        u_int_num = va_arg(arg, unsigned int);
 
-            switch(*traverse) {
-
-                case '%' :
-                    kputChar('%');
-                    break;
-                case 'c' :
-                    char_rep = va_arg(arg, int);
-                    kputChar(char_rep);
-                    break;
-                case 's' :
-                    str = va_arg(arg, const char*);
-                    kprintf(str);
-                    break;
-                case 'x' :
-                    u_int_num = va_arg(arg, unsigned int);
-                    kprintf(itoa16(u_int_num));
-                    break;
-                case 'i' :
-                    int_num = va_arg(arg, int);
-                    if(int_num == MIN_INT) { // edge case (zweier komplement who?)
-                        kprintf("-21474836478");
-                    } else {
-                        if(int_num < 0) { // if negative, print '-' and treat like unsigned int
-                            kputChar('-');
-                            int_num *= -1;
+                                        setEmptyString(c_buffer16, LEN_HEX);
+                                        kprintf(itoa16(u_int_num, c_buffer16));
+                                        break;
+                                case 'i' :
+                                        int_num = va_arg(arg, int);
+                                        if(int_num == MIN_INT) { // edge case (zweier komplement who?)
+                                                kprintf("-21474836478");
+                                        } else {
+                                                if(int_num < 0) { // if negative, print '-' and treat like unsigned int
+                                                        kputChar('-');
+                                                        int_num *= -1;
+                                                }
+                                                setEmptyString(c_buffer10, LEN_DEC);
+                                                kprintf(itoa10(int_num, c_buffer10));
+                                        }
+                                        break;
+                                case 'u' :
+                                        u_int_num = va_arg(arg, unsigned int);
+                                        setEmptyString(c_buffer10, LEN_DEC);
+                                        kprintf(itoa10(u_int_num, c_buffer10));
+                                        break;
+                                case 'p' :
+                                        kprintf("0x");
+                                        address = va_arg(arg, uint32_t); // pointer -> 32Bit address
+                                        setEmptyString(c_buffer16, LEN_HEX);
+                                        kprintf(itoa16(address, c_buffer16));
+                                        break;
+                                default: // if unknown definer, behave like printf
+                                        kputChar('%');
+                                        kputChar(*traverse);
+                                        break;
                         }
-                        kprintf(itoa10(int_num));
-                    }
-                    break;
-                case 'u' :
-                    u_int_num = va_arg(arg, unsigned int);
-                    kprintf(itoa10(u_int_num));
-                    break;
-	            case 'p' :
-	                kprintf("0x");
-                    address = va_arg(arg, uint32_t); // pointer -> 32Bit address
-                    kprintf(itoa16(address));
-                    break;
-                default: // if unknown definer, behave like printf
-                    kputChar('%');
-                    kputChar(*traverse);
-                    break;
-            }
-
-        } else {
-            kputChar(*traverse);
+                } else {
+                        kputChar(*traverse);
+                }
         }
-    }
-    va_end(arg);
-}
-
-
-
-char* itoa10(unsigned int value){
-
-    static char c_buffer[10] = ""; // 10 -> maximal amount of characters needed to present 32 bits in decimal
-    static char presentation[] = "0123456789";
-    int i = 8; // start from the back
-    do { // need do while in case value = 0
-        c_buffer[i] = presentation[value % 10];
-        --i;
-        value /= 10;
-    } while (value > 0);
-    return &c_buffer[i+1];
-}
-
-char* itoa16(unsigned int value){
-
-    static char c_buffer[8] = ""; // 8 -> maximal amount of characters needed to present 32 bits in hex
-    static char presentation[] = "0123456789abcdef";
-    int i = 6; // start from the back
-    do { // need do while in case value = 0
-        c_buffer[i] = presentation[value % 16];
-        --i;
-        value /= 16;
-    } while (value > 0);
-    return &c_buffer[i+1];
+        va_end(arg);
 }
 
