bdcec8c10dd17e2056482032d62c8ebe26d50b96
Gabo Behrendt <gabo.behrendt@gmail.com>
2019-11-08 13:21:45 +0100

    modify inputPresentation()

--- Aufgabe1/vorgabe/lib/presentations.c
+++ Aufgabe1/vorgabe/lib/presentations.c
@@ -6,8 +6,8 @@
 void inputPresentation(char receivedChar) {
         char charAsString[2] = "";
         charAsString[0] = receivedChar;
-        kprintf("Char received. Character: %c | Integer: %i | Unsigned Integer: %u \
-                | Hex: %x | String: %s | Address: %p\n\n",
+        kprintf("Char received. Character: %c | Integer: %i | Unsigned Integer: %u "
+                "| Hex: %x | String: %s | Address: %p\n\n",
                 receivedChar, receivedChar, receivedChar,
                 receivedChar, charAsString, &receivedChar);
 }
