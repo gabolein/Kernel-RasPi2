cc384a4a4be6aced99647b3dc174594fbcc117cd
Gabo Behrendt <gabo.behrendt@gmail.com>
2019-11-08 16:32:47 +0100

    rewrite functions to simple assembler calls

--- Aufgabe1/vorgabe/lib/registerDumpUtil.c
+++ Aufgabe1/vorgabe/lib/registerDumpUtil.c
@@ -32,115 +32,6 @@ static inline uint32_t getSPSR() {
         return spsr;
 }
 
-/* Get LR of Modes */
-static inline uint32_t getLR_USR() {
-        uint32_t lr_usr = 0;
-        asm volatile ("mrs %0, lr_usr": "=r" (lr_usr));
-        return lr_usr;
-}
-
-static inline uint32_t getLR_SVC() {
-        uint32_t lr_svc = 0;
-        asm volatile ("mrs %0, lr_svc": "=r" (lr_svc));
-        return lr_svc;
-}
-
-static inline uint32_t getLR_ABT() {
-        uint32_t lr_abt = 0;
-        asm volatile ("mrs %0, lr_abt": "=r" (lr_abt));
-        return lr_abt;
-}
-
-static inline uint32_t getLR_FIQ() {
-        uint32_t lr_fiq = 0;
-        asm volatile ("mrs %0, lr_fiq": "=r" (lr_fiq));
-        return lr_fiq;
-}
-
-static inline uint32_t getLR_IRQ() {
-        uint32_t lr_irq = 0;
-        asm volatile ("mrs %0, lr_irq": "=r" (lr_irq));
-        return lr_irq;
-}
-
-static inline uint32_t getLR_UND() {
-        uint32_t lr_und = 0;
-        asm volatile ("mrs %0, lr_und": "=r" (lr_und));
-        return lr_und;
-}
-
-/* Get SP of modes */
-static inline uint32_t getSP_USR() {
-        uint32_t sp_usr = 0;
-        asm volatile ("mrs %0, sp_usr": "=r" (sp_usr));
-        return sp_usr;
-}
-
-static inline uint32_t getSP_SVC() {
-        uint32_t sp_svc = 0;
-        asm volatile ("mrs %0, sp_svc": "=r" (sp_svc));
-        return sp_svc;
-}
-
-static inline uint32_t getSP_ABT() {
-        uint32_t sp_abt = 0;
-        asm volatile ("mrs %0, sp_abt": "=r" (sp_abt));
-        return sp_abt;
-}
-
-static inline uint32_t getSP_FIQ() {
-        uint32_t sp_fiq = 0;
-        asm volatile ("mrs %0, sp_fiq": "=r" (sp_fiq));
-        return sp_fiq;
-}
-
-static inline uint32_t getSP_IRQ() {
-        uint32_t sp_irq = 0;
-        asm volatile ("mrs %0, sp_irq": "=r" (sp_irq));
-        return sp_irq;
-}
-
-static inline uint32_t getSP_UND() {
-        uint32_t sp_und = 0;
-        asm volatile ("mrs %0, sp_und": "=r" (sp_und));
-        return sp_und;
-}
-
-/* Get SPSR for every mode */
-
-static inline uint32_t getSPSR_SVC() {
-        uint32_t spsr_svc = 0;
-        asm volatile ("mrs %0, spsr_svc": "=r" (spsr_svc));
-        return spsr_svc;
-}
-
-static inline uint32_t getSPSR_ABT() {
-        uint32_t spsr_abt = 0;
-        asm volatile ("mrs %0, spsr_abt": "=r" (spsr_abt));
-        return spsr_abt;
-}
-
-static inline uint32_t getSPSR_FIQ() {
-        uint32_t spsr_fiq = 0;
-        asm volatile ("mrs %0, spsr_fiq": "=r" (spsr_fiq));
-        return spsr_fiq;
-}
-
-static inline uint32_t getSPSR_IRQ() {
-        uint32_t spsr_irq = 0;
-        asm volatile ("mrs %0, spsr_irq": "=r" (spsr_irq));
-        return spsr_irq;
-}
-
-static inline uint32_t getSPSR_UND() {
-        uint32_t spsr_und = 0;
-        asm volatile ("mrs %0, spsr_und": "=r" (spsr_und));
-        return spsr_und;
-}
-
-
-
-
 char* getPSRStrings(uint32_t psrReg, char* string){
         string = "NZCV E IFT";
         /* CPSRStatus */
@@ -233,23 +124,25 @@ struct regDump* getRegDumpStruct(struct regDump* rd, enum ExceptionType exType, 
 
         rd->cpsr = getCPSR();
         rd->spsr = getSPSR();
-        rd->userLr = getLR_USR();
-        rd->userSp = getSP_USR();
-        rd->supervisorLr = getLR_SVC();
-        rd->supervisorSp = getSP_SVC();
-        rd->supervisorSpsr = getSPSR_SVC();
-        rd->abortLr = getLR_ABT();
-        rd->abortSp = getSP_ABT();
-        rd->abortSpsr = getSPSR_ABT();
-        rd->fiqLr = getLR_FIQ();
-        rd->fiqSp = getSP_FIQ();
-        rd->fiqSpsr = getSPSR_FIQ();
-        rd->irqLr = getLR_IRQ();
-        rd->irqSp = getSP_IRQ();
-        rd->irqSpsr = getSPSR_IRQ();
-        rd->undefinedLr = getLR_UND();
-        rd->undefinedSp = getSP_UND();
-        rd->undefinedSpsr = getSPSR_UND();
+
+        /* Set special registers for every mode */
+        asm volatile ("mrs %0, lr_usr" : "=r" (rd->userLr));
+        asm volatile ("mrs %0, sp_usr" : "=r" (rd->userSp));
+        asm volatile ("mrs %0, lr_svc" : "=r" (rd->supervisorLr));
+        asm volatile ("mrs %0, sp_svc" : "=r" (rd->supervisorSp));
+        asm volatile ("mrs %0, spsr_svc" : "=r" (rd->supervisorSpsr));
+        asm volatile ("mrs %0, lr_abt" : "=r" (rd->abortLr));
+        asm volatile ("mrs %0, sp_abt" : "=r" (rd->abortSp));
+        asm volatile ("mrs %0, spsr_abt" : "=r" (rd->abortSpsr));
+        asm volatile ("mrs %0, lr_fiq" : "=r" (rd->fiqLr));
+        asm volatile ("mrs %0, sp_fiq" : "=r" (rd->fiqSp));
+        asm volatile ("mrs %0, spsr_fiq" : "=r" (rd->fiqSpsr));
+        asm volatile ("mrs %0, lr_irq" : "=r" (rd->irqLr));
+        asm volatile ("mrs %0, sp_irq" : "=r" (rd->irqSp));
+        asm volatile ("mrs %0, spsr_irq" : "=r" (rd->irqSpsr));
+        asm volatile ("mrs %0, lr_und" : "=r" (rd->undefinedLr));
+        asm volatile ("mrs %0, sp_und" : "=r" (rd->undefinedSp));
+        asm volatile ("mrs %0, spsr_und" : "=r" (rd->undefinedSpsr));
 
         return rd;
 
