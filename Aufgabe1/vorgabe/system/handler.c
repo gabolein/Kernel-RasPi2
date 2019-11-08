b0b8918036f36396e9b2a18f59fb56a05cefdd7d
Thomas Fock <thomas.fock@posteo.de>
2019-11-08 10:42:09 +0100

    Add interrupt masking and unmasking to handler routines

--- Aufgabe1/vorgabe/system/handler.c
+++ Aufgabe1/vorgabe/system/handler.c
@@ -29,29 +29,51 @@ static inline uint32_t getDFARReg(){
         return dfar;
 }
 
+static inline void maskInterrupts() {
+        asm volatile("cpsid if");
+}
 
+static inline void unmaskInterrupts() {
+        asm volatile("cpsie if");
+}
 
 /* Handlers */
+/* TODO: Pending bit clearen (oder wie auch immer das heißt)
+   Prüfen um was für einen Interrupt es sich jeweils handelt
+*/
 void undefined_instruction(void* sp){
+        maskInterrupts();
         green_on();
         struct regDump rd;
         getRegDumpStruct(&rd, UNDEFINED_INSTRUCTION, sp);
+        unmaskInterrupts();
+        //mov pc, lr
 }
 void software_interrupt(void* sp){
+        maskInterrupts();
         red_on();
         struct regDump rd;
         getRegDumpStruct(&rd, UNDEFINED_INSTRUCTION, sp);
+        unmaskInterrupts();
+        //mov pc, lr
 }
 void prefetch_abort(void* sp){
+        maskInterrupts();
         struct regDump rd;
         getRegDumpStruct(&rd, UNDEFINED_INSTRUCTION, sp);
+        unmaskInterrupts();
+        //mov pc, lr
 }
 void data_abort(void* sp){
+        maskInterrupts();
         struct regDump rd;
         getRegDumpStruct(&rd, UNDEFINED_INSTRUCTION, sp);
+        unmaskInterrupts();
+        //mov pc, lr
 }
 
 void irq(){
+        maskInterrupts();
         /* Check for pending UART Interrupt */
         if(*irq_pending_2 & (uint32_t)(1 << 25)){
                 char receivedChar;
@@ -63,7 +85,12 @@ void irq(){
         kprintf("I am useless now :D. Plz end me\n");
         /* Abschmieren */
         while(1);
+        unmaskInterrupts();
+        //mov pc, lr
 }
 void fiq(){
-
+        maskInterrupts();
+        //handle das business
+        unmaskInterrupts();
+        //mov pc, lr
 }
