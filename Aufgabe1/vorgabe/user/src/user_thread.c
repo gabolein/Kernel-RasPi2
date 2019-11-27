d22b578f31b491acdf35d8f213225917341a933b
Gabo Behrendt <gabo.behrendt@gmail.com>
2019-11-27 13:14:06 +0100

    implement user thread

--- Aufgabe1/vorgabe/user/src/user_thread.c
+++ Aufgabe1/vorgabe/user/src/user_thread.c
@@ -1,6 +1,19 @@
 #include <kio.h>
+#include "handler.h"
+#include "regcheck.h"
 #include "user_thread.h"
+#include "subProgram.h"
+
+#define AMOUNT_CHARS 15
 
 void user_thread(void* arg){
-        /* Do stuff */
+        subProgramMode = 1;
+        char receivedChar = char(*arg);
+        for(int i = 0; i < AMOUNT_CHARS; i++){
+                if(checkerMode) {
+                        register_checker();
+                        checkerMode = 0;
+                }
+                kprintf("%c", receivedChar);
+                blockFunc();
 }
